9cfbb1dd986ea9c619a25f537f915ff7b3fd5702
André Ongani <90013645+Dre95160@users.noreply.github.com>
2025-06-09 01:47:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ client = clients.loc[selected_id]
 st.subheader("Informations client")
 st.write(client)
 
-API_URL = os.getenv("API_URL", "https://neo-banque-dashboard-1.onrender.com/predict")
+API_URL = os.getenv("API_URL", "https://neo-api-jigt.onrender.com/predict")
 
 if st.button("📤 Envoyer pour scoring"):
     input_data = {
