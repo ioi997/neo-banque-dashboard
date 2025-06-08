e54462f67b4d29a3bf6cd1c89be3da3878ed5b55
André Ongani <90013645+Dre95160@users.noreply.github.com>
2025-06-08 17:54:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ if st.button("📤 Envoyer pour scoring"):
     }
 
     try:
-        res = requests.post("http://localhost:8000/predict", json=input_data)
+        res = requests.post("https://neo-banque-dashboard.onrender.com", json=input_data)
         response_data = res.json()
         score = response_data["score"]
         explanations_from_api = response_data.get("explanations", [])  # clé au pluriel
